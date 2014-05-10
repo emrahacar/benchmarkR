0d514cf71832de628b7816d0e955a3c82248ed45
emrahacar <emrahacar@gmail.com>
2014-05-10 00:56:46 -0400

    first commit

---
+++ rep.R
@@ -0,0 +1,12 @@
+source('http://rbenchmark.googlecode.com/svn/trunk/benchmark.R')
+getMat<-function(file) {
+ load(file)
+ nums<-sapply(TRAIN,is.numeric)
+ TRAIN=TRAIN[,nums]
+ l<-lm(Derece ~ .-Gny-S, data=TRAIN)
+ l<-lm(Gny ~ .-Derece-S, data=TRAIN)
+ l<-lm(exp(1-S) ~ .-Derece-Gny, data=TRAIN)
+}
+
+benchmark(replications=rep(2,5),getMat('a.rda'))
+
